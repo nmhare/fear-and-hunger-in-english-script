25da9b27113b37b4a45e5b70f7db0db6c5306d31
Neil Hare <nmhare59@gmail.com>
2024-02-02 11:38:08 -0600

    Added threading Added processing and done label Made paths forward slashes over backslashes Made minimum window size Added sanity check on folder location and skipped non-json files

--- replacer.py
+++ replacer.py
@@ -3,23 +3,39 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 import os
 import json
+import threading
 
-DEFAULT_INSTALL_DIR = r"C:\Program Files (x86)\Steam\steamapps\common\Fear & Hunger\www\data"
+DEFAULT_INSTALL_DIR = "C:/Program Files (x86)/Steam/steamapps/common/Fear & Hunger/www/data"
 
 directory = DEFAULT_INSTALL_DIR
 
 def set_directory():
     global directory
+    global status_label
     directory = fd.askdirectory(
-        title="Choose your fear and hunger install", 
+        title="Choose your F&H install's /www/data folder", 
         initialdir=directory)
-    directory_label.config(text=directory)
+    if directory:
+        directory_label.config(text=directory)
+    status_label.config(text="")
+
+def start_loop_through_and_replace():
+    global directory
+    global status_label
+    if not directory.endswith("/www/data"):
+        status_label.config(text="This doesn't look like F&H /www/data folder...")
+        return
+
+    thread = threading.Thread(target=loop_through_and_replace)
+    status_label.config(text="Processing, do not close the window...")
+    thread.start()
 
 def loop_through_and_replace():
     for filename in os.listdir(directory):
         filepath = os.path.join(directory, filename)
-        if os.path.isfile(filepath):
+        if os.path.isfile(filepath) and filepath.endswith(".json"):
             find_and_replace_text(filepath)
+    status_label.config(text="done")
 
 def find_and_replace_text(filepath):
     with open(filepath, 'rb') as file:
@@ -47,12 +63,14 @@ mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
+root.minsize(width=400, height=120)
 
 directory_label = ttk.Label(mainframe, text=directory)
-set_directory_button = ttk.Button(mainframe, text="Set Directory", 
+set_directory_button = ttk.Button(mainframe, text="Set F&H /www/data Directory", 
                            command=set_directory)
 execute_button = ttk.Button(mainframe, text="Replace Text", 
-                       command=loop_through_and_replace)
+                       command=start_loop_through_and_replace)
+status_label = ttk.Label(mainframe, text="")
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
